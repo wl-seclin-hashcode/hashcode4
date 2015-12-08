cef02a4591c72e94a862be29826c77cda9569b51
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-12-08 13:11:31 +0100

    draft formatter

--- src/main/scala/hashcode/Formatter.scala
+++ src/main/scala/hashcode/Formatter.scala
@@ -6,7 +6,15 @@ object Formatter {
   def write(solution: Solution, score: Int): Unit = {
     val name = s"out.${score}.txt"
     val f = new PrintStream(name)
-    f.println(solution.toString)
+    for {
+      car <- solution.cars
+    } {
+      f.println(car.segments.size + 1)
+      f.println(1)
+      for {
+        segment <- car.segments
+      } f.println(segment)
+    }
     f.close
     println(s"wrote to $name")
   }
--- src/main/scala/hashcode/Solution.scala
+++ src/main/scala/hashcode/Solution.scala
@@ -2,6 +2,4 @@ package hashcode
 
 case class Solution(cars: List[Route])
 
-case class Route(car: Int, segments: List[Segment])
-
-case class Segment(from: Junction, to: Junction, street: Street)
+case class Route(car: Int, segments: List[Int])
