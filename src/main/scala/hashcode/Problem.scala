19dc3f06e5d7afded87dab01ca1e71c257ff297e
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-12-08 13:24:43 +0100

    validation & score

--- src/main/scala/hashcode/Problem.scala
+++ src/main/scala/hashcode/Problem.scala
@@ -4,24 +4,28 @@ case class Junction(lat: Float, long: Float, id: Int) {
 }
 
 case class Street(
-  junction1: Junction,
-  junction2: Junction,
-  bidir: Boolean,
-  cost: Int,
-  length: Int) {
+    junction1: Junction,
+    junction2: Junction,
+    bidir: Boolean,
+    cost: Int,
+    length: Int) {
   def ratio = (length + 0.0) / cost
+
+  def joins(j1: Junction, j2: Junction) =
+    (j1 == junction1 && j2 == junction2) ||
+      (j2 == junction1 && j1 == junction2 && bidir)
 }
 
 case class Problem(
-    junctions: List[Junction], 
+    junctions: List[Junction],
     streets: List[Street],
-    nbCars:Int,
-    virtualTime:Int, 
-    initialJunction:Int) {
+    nbCars: Int,
+    virtualTime: Int,
+    initialJunction: Int) {
 
   val allStreets = streets.flatMap {
     case s if s.bidir ⇒ List(s, Street(s.junction2, s.junction1, s.bidir, s.cost, s.length))
-    case s ⇒ List(s)
+    case s            ⇒ List(s)
   }
 
   val neighbours = allStreets.groupBy(_.junction1) withDefaultValue Nil
--- src/main/scala/hashcode/Validator.scala
+++ src/main/scala/hashcode/Validator.scala
@@ -5,6 +5,19 @@ import scala.util.Success
 import scala.util.Try
 
 object Validator {
-  def score(solution: Solution, problem: Problem): Try[Int] =
-    Success(0)
+  def score(solution: Solution, problem: Problem): Try[Int] = Try {
+    import problem._
+    def streetsUsed(segments: List[Int]) = for {
+      (a, b) <- (1 :: segments.init) zip segments
+      ja <- junctions.find(_.id == a)
+      jb <- junctions.find(_.id == b)
+      street <- streets.find(_.joins(ja, jb)).orElse {
+        throw new Exception(s"no street from $ja to $jb")
+      }
+    } yield street
+
+   val usedStrets= solution.cars.flatMap(r => streetsUsed(r.segments).toSet)
+   usedStrets.map(_.length).sum 
+  }
+
 }
